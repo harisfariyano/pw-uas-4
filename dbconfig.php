b56e43b8a7b652bfe167426ade303617331f93f2
harisfariyano <akungua1004@gmail.com>
2022-06-24 20:51:45 +0700

    dbcon

--- dbconfig.php
+++ dbconfig.php
@@ -2,7 +2,7 @@
 
 	global $con;
 
-	$con = mysqli_connect('localhost','root','','uaspw4');
+	$con = mysqli_connect('sql112.epizy.com','epiz_31790058','K0NJ9kM45xcsML8','epiz_31790058_wpuas');
 
 	if(!$con)
 	{
